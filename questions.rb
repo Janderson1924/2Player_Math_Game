28a8b9dbcedbb045258db17877a7b0bba35cdd22
Justin <justinanderson101@gmail.com>
2021-04-26 21:01:32 +0000

    implemented Questions class

---
+++ questions.rb
@@ -0,0 +1,10 @@
+class Questions
+  def new_question
+    random_number1 = rand(1..20)
+    random_number2 = rand(1..20)
+    puts "What does #{random_number1} + #{random_number2} equal?"
+  end
+end
+
+question1 = Questions.new
+puts question1.new_question
